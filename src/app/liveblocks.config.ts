370856bfba75764ce5fd1850832f10203b08e185
chaimaeyousfi <yousfichaimae2708@gmail.com>
2024-06-11 01:21:32 +0300

    first commit

---
+++ src/app/liveblocks.config.ts
@@ -0,0 +1,62 @@
+import { createClient } from "@liveblocks/client";
+import {LiveList, LiveObject} from "@liveblocks/core";
+import { createRoomContext } from "@liveblocks/react";
+
+const client = createClient({
+  authEndpoint: "/api/liveblocks-auth",
+  throttle: 100,
+  resolveUsers: async ({ userIds }) => {
+    const response = await fetch(`/api/users?ids=${userIds.join(',')}`);
+    return await response.json();
+  }
+});
+
+export type Presence = {
+  boardId?: null|string;
+  cardId?: null|string;
+};
+
+export type Column = {
+  name: string;
+  id: string;
+  index: number;
+};
+
+export type Card = {
+  name: string;
+  id: string;
+  index: number;
+  columnId: string;
+};
+
+type Storage = {
+  columns: LiveList<LiveObject<Column>>;
+  cards: LiveList<LiveObject<Card>>;
+};
+
+type UserMeta = {
+  id: string;
+  info: {
+    name: string;
+    email: string;
+    image: string;
+  },
+}
+
+type RoomEvent = {};
+
+type ThreadMetadata = {
+  cardId: string;
+};
+
+export const {
+  RoomProvider,
+  useMyPresence,
+  useUpdateMyPresence,
+  useStorage,
+  useMutation,
+  useRoom,
+  useSelf,
+  useOthers,
+  useThreads,
+} = createRoomContext(client);
